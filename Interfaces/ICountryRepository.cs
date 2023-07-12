de4476b20f6d43f4f39b62057675201f6a3183df
josel <joselmb17@outlook.com>
2023-07-12 22:51:27 +0100

    Created Gets and Reads for Categories and Countries

---
+++ Interfaces/ICountryRepository.cs
@@ -0,0 +1,14 @@
+﻿using PokemonReviewApp.Models;
+using System.Reflection;
+
+namespace PokemonReviewApp.Interfaces
+{
+    public interface ICountryRepository
+    {
+        ICollection<Country> GetCountries();
+        Country GetCountry(int id);
+        Country GetCountryByOwner(int OwnerId);
+        ICollection<Owner> GetOwnersFromACountry(int CountryId);
+        bool CountryExists(int id);
+    }
+}
